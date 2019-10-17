75446fcee77659802914e80844a898a4525adf44
karolis <karolis@usercentric.com.au>
2019-10-17 19:19:23 +1100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react"
+import logo from "./logo.svg"
+import axios from "axios"
+import { Flex, Text, Box, Image } from "@chakra-ui/core"
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Repo {
+  id: number
+  url: string
+  owner: { avatar_url: string; login: string }
 }
 
-export default App;
+async function fetchPopularRepos(language: string): Promise<Repo[]> {
+  var url = `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`
+  const { data } = await axios.get(url)
+  return data.items
+}
+
+const App: React.FC = () => {
+  const [state, setState] = useState<Repo[]>([])
+
+  useEffect(() => {
+    fetchPopularRepos("javaScript").then(res => {
+      setState(res)
+    })
+  }, [])
+  console.log(state)
+  return (
+    <Flex flexWrap="wrap" bg="gray.100" h="100vh">
+      {state.map(r => {
+        return (
+          <Box m="3" boxShadow="xl" bg="white">
+            <Flex>
+              <Text p="2" mx="auto">
+                {r.id}
+              </Text>
+            </Flex>
+            <Image objectFit="cover" w="40" h="40" src={r.owner.avatar_url} alt={`Avatar for ${r.owner.login}`} />
+          </Box>
+        )
+      })}
+    </Flex>
+  )
+}
+
+export default App
